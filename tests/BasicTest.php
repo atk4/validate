1ed55ef3984cb24158a930b981eb2c56e2ab9cec
Francesco Danti <fdanti@gmail.com>
2022-02-06 09:49:13 +0100

    Fix max connection

--- tests/BasicTest.php
+++ tests/BasicTest.php
@@ -23,10 +23,16 @@ class BasicTest extends TestCase
                 ['id' => 1, 'name' => 'ball', 'type' => 'ball', 'tail_length' => 0, 'age' => 30],
             ],
         ]);
+    }
 
-        $this->m = new ModelTest($this->db);
+    protected function getModel(): Model
+    {
+        return new ModelTest($this->db);
+    }
 
-        $this->c = new Validator($this->m);
+    protected function getValidator(Model $model): Validator
+    {
+        return new Validator($model);
     }
 
     /**
@@ -34,9 +40,12 @@ class BasicTest extends TestCase
      */
     public function testSimple1(): void
     {
-        $this->c->rule('name', ['required', ['lengthMin', 3]]);
+        $model = $this->getModel();
+        $validator = $this->getValidator($model);
 
-        $err = $this->m->createEntity()->set('name', 'a')->validate();
+        $validator->rule('name', ['required', ['lengthMin', 3]]);
+
+        $err = $model->createEntity()->set('name', 'a')->validate();
         $this->assertSame(['name'], array_keys($err));
     }
 
@@ -45,9 +54,12 @@ class BasicTest extends TestCase
      */
     public function testSimple2(): void
     {
-        $this->c->rule('name', ['required', ['lengthMin', 3]]);
+        $model = $this->getModel();
+        $validator = $this->getValidator($model);
 
-        $err = $this->m->createEntity()->set('name', 'a')->validate();
+        $validator->rule('name', ['required', ['lengthMin', 3]]);
+
+        $err = $model->createEntity()->set('name', 'a')->validate();
         $this->assertSame(['name'], array_keys($err));
     }
 
@@ -56,13 +68,16 @@ class BasicTest extends TestCase
      */
     public function testMultiple1(): void
     {
-        $this->c->rules([
+        $model = $this->getModel();
+        $validator = $this->getValidator($model);
+
+        $validator->rules([
             'name' => ['required'],
             'age' => ['integer', ['min', 0], ['max', 99]],
             'tail_length' => ['integer', ['min', 0]],
         ]);
 
-        $err = $this->m->createEntity()->setMulti([
+        $err = $model->createEntity()->setMulti([
             'name' => null,
             'age' => 10,
             'tail_length' => 5.45,
@@ -76,8 +91,11 @@ class BasicTest extends TestCase
      */
     public function testCallback1(): void
     {
+        $model = $this->getModel();
+        $validator = $this->getValidator($model);
+
         // Age should be odd (nepāra skaitlis)
-        $this->c->rule('age', [
+        $validator->rule('age', [
             [
                 function ($field, $value, $params, $data) {
                     return $value % 2 !== 0;
@@ -86,7 +104,7 @@ class BasicTest extends TestCase
             ],
         ]);
 
-        $err = $this->m->createEntity()->setMulti([
+        $err = $model->createEntity()->setMulti([
             'age' => 10, // odd number, so should throw error
         ])->validate();
 
@@ -98,7 +116,10 @@ class BasicTest extends TestCase
      */
     public function testIf(): void
     {
-        $this->c->if(['type' => 'dog'], [
+        $model = $this->getModel();
+        $validator = $this->getValidator($model);
+
+        $validator->if(['type' => 'dog'], [
             // dogs require age and tail_length
             'age' => ['required'],
             'tail_length' => ['required'],
@@ -108,20 +129,20 @@ class BasicTest extends TestCase
         ]);
 
         // ball don't require tail_length and age
-        $err = $this->m->createEntity()->setMulti([
+        $err = $model->createEntity()->setMulti([
             'type' => 'ball',
         ])->validate();
         $this->assertSame([], array_keys($err));
 
         // ball should not have tail_length
-        $err = $this->m->createEntity()->setMulti([
+        $err = $model->createEntity()->setMulti([
             'type' => 'ball',
             'tail_length' => 5,
         ])->validate();
         $this->assertSame(['tail_length'], array_keys($err));
 
         // dogs require age and tail_length
-        $err = $this->m->createEntity()->setMulti([
+        $err = $model->createEntity()->setMulti([
             'type' => 'dog',
             'tail_length' => 5, // age is not set
         ])->validate();
@@ -133,41 +154,44 @@ class BasicTest extends TestCase
      */
     public function testMix(): void
     {
-        $this->c->rule('age', [['min', 3]]); // everything should have age at least 3
-        $this->c->if(['type' => 'dog'], [
+        $model = $this->getModel();
+        $validator = $this->getValidator($model);
+
+        $validator->rule('age', [['min', 3]]); // everything should have age at least 3
+        $validator->if(['type' => 'dog'], [
             // dogs require age and age of dog should be less than 20
             'age' => ['required', ['max', 20]],
         ]);
 
-        $err = $this->m->createEntity()->setMulti([
+        $err = $model->createEntity()->setMulti([
             'type' => 'ball',
         ])->validate();
         $this->assertSame([], array_keys($err)); // age can be blank for balls
 
-        $err = $this->m->createEntity()->setMulti([
+        $err = $model->createEntity()->setMulti([
             'type' => 'ball',
             'age' => 2,
         ])->validate();
         $this->assertSame(['age'], array_keys($err)); // age must be at least 3 for everything if set
 
-        $err = $this->m->createEntity()->setMulti([
+        $err = $model->createEntity()->setMulti([
             'type' => 'dog',
         ])->validate();
         $this->assertSame(['age'], array_keys($err)); // for dogs age is required
 
-        $err = $this->m->createEntity()->setMulti([
+        $err = $model->createEntity()->setMulti([
             'type' => 'dog',
             'age' => 10,
         ])->validate();
         $this->assertSame([], array_keys($err)); // for dogs age 10 is ok
 
-        $err = $this->m->createEntity()->setMulti([
+        $err = $model->createEntity()->setMulti([
             'type' => 'dog',
             'age' => 2,
         ])->validate();
         $this->assertSame(['age'], array_keys($err)); // for dogs also age should be at least 3
 
-        $err = $this->m->createEntity()->setMulti([
+        $err = $model->createEntity()->setMulti([
             'type' => 'dog',
             'age' => 30,
         ])->validate();
@@ -179,17 +203,20 @@ class BasicTest extends TestCase
      */
     public function testMessage(): void
     {
-        $this->c->rule('age', [
+        $model = $this->getModel();
+        $validator = $this->getValidator($model);
+
+        $validator->rule('age', [
             ['min', 3, 'message' => 'Common! {field} to small'],
             ['max', 5, 'message' => 'And now to big'],
         ]);
 
-        $err = $this->m->createEntity()->setMulti([
+        $err = $model->createEntity()->setMulti([
             'age' => 2,
         ])->validate();
         $this->assertSame(['age' => 'Common! Age to small'], $err); // custom message here
 
-        $err = $this->m->createEntity()->setMulti([
+        $err = $model->createEntity()->setMulti([
             'age' => 10,
         ])->validate();
         $this->assertSame(['age' => 'And now to big'], $err); // custom message here
@@ -197,9 +224,12 @@ class BasicTest extends TestCase
 
     public function testModelHookValidate(): void
     {
-        $entity = $this->m->createEntity();
+        $model = $this->getModel();
+        $validator = $this->getValidator($model);
 
-        $this->c->rule('name', ['required', ['lengthMin', 3]]);
+        $entity = $model->createEntity();
+
+        $validator->rule('name', ['required', ['lengthMin', 3]]);
 
         $entity->setMulti([
             'name' => 'abcd',
