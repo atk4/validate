b84cc8da783076b8d414113a09517a855bf13649
DarkSide <imants.horsts@inbox.lv>
2024-04-15 20:29:52 +0300

    cs fix

--- src/ValitronValidator.php
+++ src/ValitronValidator.php
@@ -13,34 +13,37 @@ class ValitronValidator extends OriginalValidator
 {
     protected function validateDate($field, $value)
     {
-        if (is_null($value) || (is_string($value) && trim($value) === '')) {
+        if ($value === null || (is_string($value) && trim($value) === '')) {
             return false;
         }
 
         return parent::validateDate($field, $value);
     }
 
+    /** @param array<mixed> $params */
     protected function validateDateFormat($field, $value, $params)
     {
-        if (is_null($value) || (is_string($value) && trim($value) === '')) {
+        if ($value === null || (is_string($value) && trim($value) === '')) {
             return false;
         }
 
         return parent::validateDateFormat($field, $value, $params);
     }
 
+    /** @param array<mixed> $params */
     protected function validateDateBefore($field, $value, $params)
     {
-        if (is_null($value) || (is_string($value) && trim($value) === '')) {
+        if ($value === null || (is_string($value) && trim($value) === '')) {
             return false;
         }
 
         return parent::validateDateBefore($field, $value, $params);
     }
 
+    /** @param array<mixed> $params */
     protected function validateDateAfter($field, $value, $params)
     {
-        if (is_null($value) || (is_string($value) && trim($value) === '')) {
+        if ($value === null || (is_string($value) && trim($value) === '')) {
             return false;
         }
 
